a32a3491ba58224f1a4471494e61e74d857d6446
Marco Romero <maromero@atsistemas.com>
2018-10-23 21:08:16 +0200

    [add] date function and styles footer

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,11 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'c-footer',
   templateUrl: 'footer.html'
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-    ngOnInit(){}
- 
+    private date = new Date();
+
+    public hour: number;
+    public minute: number;
+    public second: number;
+
+    constructor(){
+
+      setInterval(() => {
+        this.date = new Date();
+        this.hour = formatDate(this.date.getHours());
+        this.minute = formatDate(this.date.getMinutes());
+        this.second = formatDate(this.date.getSeconds());
+      }, 1);
+
+    }; 
+
+}
+
+/**
+ * Check if the hour & minute & second is < 10 to format it and put a 0 in front
+ * of it.
+ * @param a 
+ * @return a 
+ */
+function formatDate(a) : number{
+  return a = (a < 10) ? ("0" + a) : a;
 }
